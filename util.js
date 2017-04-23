5346ad35406c35d830c2a6e3dfaf39dae251d939
MultitudesOfMinions <multitudes.of.minions@gmail.com>
2017-04-23 17:19:06 -0500

    Create util.js

---
+++ util.js
@@ -0,0 +1,11 @@
+function point(x, y){ this.x = x||0; this.y = y||0; }
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function DistanceSquared(P1, P2){
+	return (P1.y - P2.y)**2 - (P1.x - P2.x)**2;
+}
