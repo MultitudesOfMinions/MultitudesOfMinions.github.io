e6f1969e1ee71a4d492fbd6512670cf4bb00ff4a
MultitudesOfMinions <multitudes.of.minions@gmail.com>
2017-04-25 07:18:51 -0500

    Update Minion.js

--- Minion.js
+++ Minion.js
@@ -7,7 +7,7 @@ function Minion(hp, damage, moveSpeed){
 	this.hp = hp||10;
 	this.damage = damage||0;
 	this.moveSpeed = moveSpeed||1;
-	this.Location = new point(path[0].x, path[0].y);
+	this.Location = new point(-halfW, halfH);
 }
 Minion.prototype.Move = function(){
 	for(var i=0;i<path.length;i++){
