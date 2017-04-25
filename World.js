a870aae55ba3a311949e62c38c9aadad5fcc685b
MultitudesOfMinions <multitudes.of.minions@gmail.com>
2017-04-24 21:02:01 -0500

    Update World.js
    added a minion!

--- World.js
+++ World.js
@@ -1,35 +1,57 @@
-var pageW = Math.max(document.documentElement.clientWidth, window.innerWidth || 0) - 30;
-var pageH = (Math.max(document.documentElement.clientHeight, window.innerHeight || 0) - 30) / 4;
-var halfW = pageW/2;
-var halfH = pageH/2;
+var gameW = Math.max(document.documentElement.clientWidth, window.innerWidth || 0) - 30;
+var gameH = (Math.max(document.documentElement.clientHeight, window.innerHeight || 0) - 30) / 4;
+var halfW = gameW>>1;
+var halfH = gameH>>1;
 var drawArea = document.getElementById('canvasArea');
-drawArea.width = pageW;
-drawArea.height = pageH;
+drawArea.width = gameW;
+drawArea.height = gameH;
 var ctx = drawArea.getContext('2d');
 var path = [];
-var interval;
+var sin = [];
+var cos = [];
+var pathInc = gameW>>6;
+var minions = [];
+var mainCycle;
 
 function init(){
-	path[0] = new point(-100, 0);
-	while(path.length > 0 && path[path.length - 1].x < pageW + 100){
+	path[0] = new point(-100, halfH);
+	while(path.length > 0 && path[path.length - 1].x < gameW + 100){
 		addPathPoint();
 	}
 	
-	interval = setInterval(update, 20);
+	mainCycle = setInterval(update, 20);
+	minions[0] = new Minion(10, 10 ,1, path[0].x, path[0].y);
+	
+	//build cos/sin look up tables to speed up minion movement later.
+	for(var i=(-gameH>>5)-5; i<(gameH>>5)+6;i++){
+		sin[sin.length]=new point(i,-Math.sin(i/pathInc));
+		cos[cos.length]=new point(i,Math.cos(i/pathInc));
+	}
 }
 
 function update(){
 	ctx.fillStyle='#000';
-	ctx.fillRect(0,0, pageW, pageH);
+	ctx.fillRect(0,0, gameW, gameH);
 	
 	//Add more path if needed.
-	while(path.length > 0 && path[path.length - 1].x < pageW + 100){ addPathPoint(); }
+	while(path.length > 0 && path[path.length - 1].x < gameW + 100){
+		addPathPoint();
+	}
 	
 	//Remove past path points
-	while(path[0].x < -100){ path.splice(0,1); }
+	while(path[0].x < -100){
+		path.splice(0,1);
+	}
 	
-	//Test infinite path. Remove later.
-	for(var i=0; i < path.length; i++){ path[i].x -= 5; }
+	var maxX = 0;
+	for(var i=0;i<minions.length;i++){ maxX = Math.max(maxX, minions[i].Location.x); }
+	if(maxX > halfW){
+		var deltaX = maxX - halfW;
+		for(var i=0; i < path.length; i++){ path[i].x -= deltaX; }
+		for(var i=0; i < minions.length; i++){ minions[i].Location.x -= deltaX; }
+	}
+	
+	for(var i=0;i<minions.length;i++){ minions[i].Move(); }
 	
 	drawPath();
 	drawMinions();
@@ -38,16 +60,20 @@ function update(){
 
 function addPathPoint(){
 	var lastPoint = path[path.length - 1];
+	
 	var skew = (halfH - lastPoint.y) >> 4;//Keep path towards center.
-	var delta = getRandomInt((pageH/-20) + skew, (pageH/20) + skew);
-	var newX = lastPoint.x + (pageW / 100);
+	
+	var delta = getRandomInt((-gameH>>5) + 1, (gameH>>5));
+	console.log(skew + ":" + delta);
+	delta += skew;
+	var newX = lastPoint.x + pathInc;
 	var newY = lastPoint.y + delta;
 	
 	path[path.length] = new point(newX, newY); //Add a new point
 }
 
 function drawPath(){
-	var pathW = pageH / 15;
+	var pathW = gameH / 15;
 	
 	ctx.beginPath();
 	ctx.lineWidth = pathW + 5;
@@ -62,11 +88,17 @@ function drawPath(){
 	ctx.lineWidth = pathW;
 	ctx.strokeStyle = '#FFF';
 	ctx.moveTo(path[0].x, path[0].y);
-	for(var i=1;i<path.length;i++){ ctx.lineTo(path[i].x, path[i].y); }
+	for(var i=1;i<path.length;i++){
+		ctx.lineTo(path[i].x, path[i].y);
+	}
 	ctx.stroke();
 }
 
-function drawMinions(){}
+function drawMinions(){ 
+	for(var i=0;i<minions.length;i++){ 
+		minions[i].Draw(); 
+	}
+}
 
 function drawTowers() {}
 
